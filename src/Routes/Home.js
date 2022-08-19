f9be314d1fde3deb2718d456717d5863d30afb3e
zliel <zpliel@gmail.com>
2022-08-18 22:57:00 -0700

    Added a boilerplate home page

---
+++ src/Routes/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Box, Typography} from "@mui/material";
+
+function Home(props) {
+    return (
+        <Box>
+            <Typography variant={"h6"}>Hello!</Typography>
+        </Box>
+    );
+}
+
+export default Home;
