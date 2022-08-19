2906b324046da6d8bb8ded5999f849bc8647f779
zliel <zpliel@gmail.com>
2022-08-19 03:57:25 -0700

    Added the Recipe Scraper to the Home page

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Box, Typography} from "@mui/material";
+import RecipeScraper from "../Components/RecipeScraper";
 
 function Home(props) {
     return (
         <Box>
+            <RecipeScraper />
             <Typography variant={"h6"}>Hello!</Typography>
         </Box>
     );
